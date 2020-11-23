bf8b4a1ef3deda9310c21bb476588cd99cc83ffd
YairHanimov <hthtr0@gmail.com>
2020-11-23 15:58:11 +0200

    code of ball ditaction

---
+++ app/src/main/java/com/yair/amshu/Coloralgo.java
@@ -0,0 +1,107 @@
+package com.yair.amshu;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+public class Coloralgo {
+    // Lower and Upper bounds for range checking in HSV color space
+    private Scalar LowBound = new Scalar(0);
+    private Scalar UpBound = new Scalar(0);
+    // Minimum contour area in percent for contours filtering
+    private static double minarea = 0.1;
+    // Color radius for range checking in HSV color space
+    private Scalar ColorRadius = new Scalar(25,50,50,0);
+    private Mat Spectrumm = new Mat();
+    private List<MatOfPoint> mContours = new ArrayList<MatOfPoint>();
+
+    // Cache
+    Mat PyrDownMat = new Mat();
+    Mat HsvMat = new Mat();
+    Mat Mask = new Mat();
+    Mat DilatedMask = new Mat();
+    Mat Hierarchy = new Mat();
+
+    public void setColorRadius(Scalar radius) {
+        ColorRadius = radius;
+    }
+
+    public void setHsvColor(Scalar hsvColor) {
+        double minH = (hsvColor.val[0] >= ColorRadius.val[0]) ? hsvColor.val[0]- ColorRadius.val[0] : 0;
+        double maxH = (hsvColor.val[0]+ ColorRadius.val[0] <= 255) ? hsvColor.val[0]+ ColorRadius.val[0] : 255;
+
+        LowBound.val[0] = minH;
+        UpBound.val[0] = maxH;
+
+        LowBound.val[1] = hsvColor.val[1] - ColorRadius.val[1];
+        UpBound.val[1] = hsvColor.val[1] + ColorRadius.val[1];
+
+        LowBound.val[2] = hsvColor.val[2] - ColorRadius.val[2];
+        UpBound.val[2] = hsvColor.val[2] + ColorRadius.val[2];
+
+        LowBound.val[3] = 0;
+        UpBound.val[3] = 255;
+
+        Mat spectrumHsv = new Mat(1, (int)(maxH-minH), CvType.CV_8UC3);
+
+        for (int j = 0; j < maxH-minH; j++) {
+            byte[] tmp = {(byte)(minH+j), (byte)255, (byte)255};
+            spectrumHsv.put(0, j, tmp);
+        }
+
+        Imgproc.cvtColor(spectrumHsv, Spectrumm, Imgproc.COLOR_HSV2RGB_FULL, 4);
+    }
+
+    public Mat getSpectrum() {
+        return Spectrumm;
+    }
+
+    public void setMinContourArea(double area) {
+        minarea = area;
+    }
+
+    public void process(Mat rgbaImage) {
+        Imgproc.pyrDown(rgbaImage, PyrDownMat);
+        Imgproc.pyrDown(PyrDownMat, PyrDownMat);
+
+        Imgproc.cvtColor(PyrDownMat, HsvMat, Imgproc.COLOR_RGB2HSV_FULL);
+
+        Core.inRange(HsvMat, LowBound, UpBound, Mask);
+        Imgproc.dilate(Mask, DilatedMask, new Mat());
+
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+
+        Imgproc.findContours(DilatedMask, contours, Hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        // Find max contour area
+        double maxArea = 0;
+        Iterator<MatOfPoint> each = contours.iterator();
+        while (each.hasNext()) {
+            MatOfPoint wrapper = each.next();
+            double area = Imgproc.contourArea(wrapper);
+            if (area > maxArea)
+                maxArea = area;
+        }
+
+        // Filter contours by area and resize to fit the original image size
+        mContours.clear();
+        each = contours.iterator();
+        while (each.hasNext()) {
+            MatOfPoint contour = each.next();
+            if (Imgproc.contourArea(contour) > minarea *maxArea) {
+                Core.multiply(contour, new Scalar(4,4), contour);
+                mContours.add(contour);
+            }
+        }
+    }
+
+    public List<MatOfPoint> getContours() {
+        return mContours;
+    }
+}
--- app/src/main/java/com/yair/amshu/MainActivity.java
+++ app/src/main/java/com/yair/amshu/MainActivity.java
@@ -1,14 +1,194 @@
 package com.yair.amshu;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.List;
+
+public class MainActivity extends Activity implements View.OnTouchListener, CameraBridgeViewBase.CvCameraViewListener2 {
+    private static final String  TAG              = "MainActivity";
+
+    private boolean colorselect = false;
+    private Mat thergba, thespectrum;
+    private Scalar ballcolorrgb, ballcolorhsv, counter;
+    private Coloralgo ditaction;
+    private Size spectorsize;
+
+    private CameraBridgeViewBase opencvcam;
+
+    private BaseLoaderCallback theLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    opencvcam.enableView();
+                    opencvcam.setOnTouchListener(MainActivity.this);
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    public MainActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_main);
+
+        opencvcam = (CameraBridgeViewBase) findViewById(R.id.mycamera);
+        opencvcam.setVisibility(SurfaceView.VISIBLE);
+        opencvcam.setCvCameraViewListener(this);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onPause()
+    {
+        super.onPause();
+        if (opencvcam != null)
+            opencvcam.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, theLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            theLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (opencvcam != null)
+            opencvcam.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+        thergba = new Mat(height, width, CvType.CV_8UC4);
+        ditaction = new Coloralgo();
+        thespectrum = new Mat();
+        ballcolorrgb = new Scalar(255);
+        ballcolorhsv = new Scalar(255);
+        spectorsize = new Size(200, 64);
+        counter = new Scalar(255,0,0,255);
+    }
+
+    public void onCameraViewStopped() {
+        thergba.release();
+    }
+
+    public boolean onTouch(View v, MotionEvent event) {
+        int cols = thergba.cols();
+        int rows = thergba.rows();
+
+        int xOffset = (opencvcam.getWidth() - cols) / 2;
+        int yOffset = (opencvcam.getHeight() - rows) / 2;
+
+        int x = (int)event.getX() - xOffset;
+        int y = (int)event.getY() - yOffset;
+
+        Log.i(TAG, "Touch image coordinates: (" + x + ", " + y + ")");
+
+        if ((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
+
+        Rect touchedRect = new Rect();
+
+        touchedRect.x = (x>4) ? x-4 : 0;
+        touchedRect.y = (y>4) ? y-4 : 0;
+
+        touchedRect.width = (x+4 < cols) ? x + 4 - touchedRect.x : cols - touchedRect.x;
+        touchedRect.height = (y+4 < rows) ? y + 4 - touchedRect.y : rows - touchedRect.y;
+
+        Mat touchedRegionRgba = thergba.submat(touchedRect);
+
+        Mat touchedRegionHsv = new Mat();
+        Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
+
+        // Calculate average color of touched region
+        ballcolorhsv = Core.sumElems(touchedRegionHsv);
+        int pointCount = touchedRect.width*touchedRect.height;
+        for (int i = 0; i < ballcolorhsv.val.length; i++)
+            ballcolorhsv.val[i] /= pointCount;
+
+        ballcolorrgb = converScalarHsv2Rgba(ballcolorhsv);
+
+        Log.i(TAG, "Touched rgba color: (" + ballcolorrgb.val[0] + ", " + ballcolorrgb.val[1] +
+                ", " + ballcolorrgb.val[2] + ", " + ballcolorrgb.val[3] + ")");
+
+        ditaction.setHsvColor(ballcolorhsv);
+
+        Imgproc.resize(ditaction.getSpectrum(), thespectrum, spectorsize, 0, 0, Imgproc.INTER_LINEAR_EXACT);
+
+        colorselect = true;
+
+        touchedRegionRgba.release();
+        touchedRegionHsv.release();
+
+        return false; // don't need subsequent touch events
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        thergba = inputFrame.rgba();
+
+        if (colorselect) {
+            ditaction.process(thergba);
+            List<MatOfPoint> contours = ditaction.getContours();
+            Log.e(TAG, "Contours count: " + contours.size());
+            Imgproc.drawContours(thergba, contours, -1, counter);
+
+            Mat colorLabel = thergba.submat(4, 68, 4, 68);
+            colorLabel.setTo(ballcolorrgb);
+
+            Mat spectrumLabel = thergba.submat(4, 4 + thespectrum.rows(), 70, 70 + thespectrum.cols());
+            thespectrum.copyTo(spectrumLabel);
+        }
+
+        return thergba;
+    }
+
+    private Scalar converScalarHsv2Rgba(Scalar hsvColor) {
+        Mat pointMatRgba = new Mat();
+        Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
+        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
+
+        return new Scalar(pointMatRgba.get(0, 0));
     }
 }
+
